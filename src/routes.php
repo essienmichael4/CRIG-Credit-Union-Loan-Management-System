d18dea955bac76a21c3315c631f6fdedad40a618
Code_Junior <46490572+essienmichael4@users.noreply.github.com>
2022-04-29 13:03:56 +0100

    Create routes.php

---
+++ src/routes.php
@@ -0,0 +1,33 @@
+<?php
+// Start of user session
+    session_start();
+
+    // Checking if user session is available, if not routes you back to the login page on line 7
+    if(!$_SESSION["uid"]){
+        header("location: ../index.php");
+    }else{
+        // Routing of the web application after sucessful login
+        require("header.php");// gets header file
+
+        // Setting of initial page name
+        $controller = "dashboard";
+
+        // Checks if the page name is set, if not sets it.
+        if(isset($_GET['pgname'])){
+            $controller = $_GET['pgname'];
+            $controller = strtolower($controller);
+        }else if(!isset($_GET['pgname'])){
+            $controller = strtolower($controller);
+        }
+
+        //Checks if file exists and routes to the veiw
+        if(file_exists("./".$controller.".php")){
+            require("./".$controller.".php");
+        }else{
+            $controller = "404";
+            require("./".$controller.".php");
+        }
+
+        require("footer.php");// gets footer file
+    }
+?>
