cdb964dd9905dcd51ee965089ed4f70c6ba2f690
Code_Junior <46490572+essienmichael4@users.noreply.github.com>
2022-07-03 06:52:23 +0000

    Create activatemember.inc.php

---
+++ php/savings/activatemember.inc.php
@@ -0,0 +1,16 @@
+<?php
+    $id = $_POST["id"];
+    $user = $_POST["user"];
+    include_once("../dbs.inc.php");
+    $status = "active";
+    $date = date('Y-m-d h:i:s');
+
+    $sql = "UPDATE `savings` SET `account_status` = '{$status}' WHERE `id` = {$id};";
+    if(mysqli_query($conn, $sql)){
+        echo "success";
+    }else{
+        echo "failure";
+    }
+
+    // , `disabled_by` = '{$user}',
+    // `day_disabled` = '{$date}'
