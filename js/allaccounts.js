7370dcd5d0c86412b19e12fd2f89a260e9ec9ffa
Code_Junior <46490572+essienmichael4@users.noreply.github.com>
2022-07-03 06:52:39 +0000

    Create allaccounts.js

---
+++ js/allaccounts.js
@@ -0,0 +1,89 @@
+window.addEventListener("load",()=>{
+    let table = document.querySelector(".savings__table");
+
+    let xhr = new XMLHttpRequest();
+    xhr.open("GET", "../php/savings/allaccounts.inc.php", true);
+    xhr.onload = () =>{
+        if(xhr.readyState == XMLHttpRequest.DONE){
+            if(xhr.status == 200){
+                let data = xhr.response;
+                table.innerHTML = data;
+            }
+        }
+    }
+    xhr.send()
+})
+
+const getallaccounts = document.querySelector(".getallaccounts")
+const getalldeposit = document.querySelector(".getalldeposit")
+const getalltransactions = document.querySelector(".getalltransactions")
+const getalldebit = document.querySelector(".getalldebit")
+let account_title = document.querySelector(".account-title")
+
+getallaccounts.addEventListener("click",()=>{
+    let table = document.querySelector(".savings__table");
+
+    let xhr = new XMLHttpRequest();
+    xhr.open("GET", "../php/savings/allaccounts.inc.php", true);
+    xhr.onload = () =>{
+        if(xhr.readyState == XMLHttpRequest.DONE){
+            if(xhr.status == 200){
+                let data = xhr.response;
+                table.innerHTML = data;
+                account_title.textContent = "Accounts";
+            }
+        }
+    }
+    xhr.send()
+})
+
+getalldeposit.addEventListener("click",()=>{
+    let table = document.querySelector(".savings__table");
+
+    let xhr = new XMLHttpRequest();
+    xhr.open("GET", "../php/savings/getallaccountsdeposit.inc.php", true);
+    xhr.onload = () =>{
+        if(xhr.readyState == XMLHttpRequest.DONE){
+            if(xhr.status == 200){
+                let data = xhr.response;
+                table.innerHTML = data;
+                account_title.textContent = "Deposits";
+            }
+        }
+    }
+    xhr.send()
+})
+
+getalldebit.addEventListener("click",()=>{
+    let table = document.querySelector(".savings__table");
+
+    let xhr = new XMLHttpRequest();
+    xhr.open("GET", "../php/savings/getallaccountsdebit.inc.php", true);
+    xhr.onload = () =>{
+        if(xhr.readyState == XMLHttpRequest.DONE){
+            if(xhr.status == 200){
+                let data = xhr.response;
+                table.innerHTML = data;
+                account_title.textContent = "Withdrawals";
+            }
+        }
+    }
+    xhr.send()
+})
+
+getalltransactions.addEventListener("click",()=>{
+    let table = document.querySelector(".savings__table");
+
+    let xhr = new XMLHttpRequest();
+    xhr.open("GET", "../php/savings/getallaccountstransactions.inc.php", true);
+    xhr.onload = () =>{
+        if(xhr.readyState == XMLHttpRequest.DONE){
+            if(xhr.status == 200){
+                let data = xhr.response;
+                table.innerHTML = data;
+                account_title.textContent = "Transactions";
+            }
+        }
+    }
+    xhr.send()
+})
